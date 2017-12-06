3d03ce5afb1f99778ce58ad464f0601341cc9423
Tom Spencer <tom.spencer@studiozeffa.com>
2017-12-06 12:32:12 +0000

    Nudge headerbar down on Android

--- src/navigator.js
+++ src/navigator.js
@@ -1,7 +1,18 @@
+import { Constants } from 'expo';
+import { Platform } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 import ListScreen from './screens/ListScreen';
 
-export default StackNavigator({
-  List: { screen: ListScreen }
-});
+export default StackNavigator(
+  {
+    List: { screen: ListScreen }
+  },
+  {
+    navigationOptions: {
+      headerStyle: {
+        marginTop: Platform.OS === 'android' ? Constants.statusBarHeight : 0
+      }
+    }
+  }
+);
