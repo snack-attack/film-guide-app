ee8760bf4568a02f331871fa9d3cd7e9b6e4fb08
Tom Spencer <tom.spencer@studiozeffa.com>
2017-12-07 10:49:33 +0000

    Added list of showtimes

--- src/components/FilmDetail.js
+++ src/components/FilmDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { dimensions, fontSizes, lineHeights } from '../theme';
+import { dimensions, fontSizes, lineHeights, colors } from '../theme';
 
 const GUTTER = dimensions.basePadding / 2;
 
@@ -10,6 +10,15 @@ const FilmDetail = ({ film }) => (
       {!!film.year && <Text style={styles.stats}>Released in {film.year}</Text>}
       {!!film.tmdbRating && <Text style={styles.stats}>Rating: {film.tmdbRating}%</Text>}
     </View>
+    <View style={styles.showtimesContainer}>
+      <Text style={styles.showtimesHeader}>Showtimes:</Text>
+      {film.showtimes.map(showtime => (
+        <Text style={styles.showtime}>
+          {showtime.startsAtDate} at {showtime.startsAtTime} on {showtime.channel}
+        </Text>
+      ))}
+    </View>
+    <View style={styles.separator} />
     <Text style={styles.synopsis}>{film.synopsis}</Text>
   </View>
 );
@@ -31,9 +40,26 @@ const styles = StyleSheet.create({
     lineHeight: lineHeights.normal,
     paddingVertical: GUTTER
   },
+  showtimesContainer: {
+    paddingVertical: GUTTER
+  },
+  showtimesHeader: {
+    fontSize: fontSizes.normal,
+    lineHeight: lineHeights.normal
+  },
+  showtime: {
+    fontSize: fontSizes.small,
+    lineHeight: lineHeights.small,
+    paddingTop: GUTTER
+  },
   synopsis: {
     fontSize: fontSizes.small,
     lineHeight: lineHeights.small,
     paddingVertical: GUTTER
+  },
+  separator: {
+    backgroundColor: colors.concrete,
+    height: 1,
+    marginVertical: GUTTER
   }
 });
