cd579b03ec661389d4421182f7604fe62aa82a11
Tom Spencer <tom.spencer@studiozeffa.com>
2017-12-08 21:22:32 +0000

    Added parallax scrolling on Film Detail

--- src/components/FilmDetail.js
+++ src/components/FilmDetail.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import ParallaxScrollView from 'react-native-parallax-scroll-view';
 import Image from 'react-native-image-progress';
 import { dimensions, fontSizes, lineHeights, colors } from '../theme';
 import FilmRating from './FilmRating';
 import FilmShowtime from './FilmShowtime';
 
 const GUTTER = dimensions.basePadding / 2;
+const IMAGE_ASPECT_RATIO = 0.75;
 
-const FilmDetail = ({ film }) => (
-  <View style={styles.container}>
-    <ScrollView>
-      {!!film.tmdbImageId && (
-        <Image
-          style={styles.image}
-          source={{
-            uri: `https://image.tmdb.org/t/p/original/${film.tmdbImageId}.jpg`
-          }}
-        />
-      )}
+class FilmDetail extends Component {
+  constructor() {
+    super();
+
+    this.renderImage = this.renderImage.bind(this);
+  }
+
+  renderTextContent() {
+    const { film } = this.props;
+    return (
       <View style={styles.textContainer}>
         <View style={styles.statsContainer}>
           {!!film.year && <Text style={styles.stats}>Released in {film.year}</Text>}
@@ -36,9 +37,44 @@ const FilmDetail = ({ film }) => (
         <View style={styles.separator} />
         <Text style={styles.synopsis}>{film.synopsis}</Text>
       </View>
-    </ScrollView>
-  </View>
-);
+    );
+  }
+
+  renderImage() {
+    const { film } = this.props;
+    return (
+      <Image
+        style={styles.image}
+        source={{
+          uri: `https://image.tmdb.org/t/p/original/${film.tmdbImageId}.jpg`
+        }}
+      />
+    );
+  }
+
+  renderWithImage() {
+    const { width: deviceWidth } = Dimensions.get('window');
+    const parallaxHeaderHeight = deviceWidth / IMAGE_ASPECT_RATIO;
+    return (
+      <ParallaxScrollView
+        parallaxHeaderHeight={parallaxHeaderHeight}
+        outputScaleValue={2}
+        backgroundColor={colors.white}
+        renderBackground={this.renderImage}
+      >
+        {this.renderTextContent()}
+      </ParallaxScrollView>
+    );
+  }
+
+  renderNoImage() {
+    return <ScrollView>{this.renderTextContent()}</ScrollView>;
+  }
+  render() {
+    const { film } = this.props;
+    return <View style={styles.container}>{film.tmdbImageId ? this.renderWithImage() : this.renderNoImage()}</View>;
+  }
+}
 
 export default FilmDetail;
 
