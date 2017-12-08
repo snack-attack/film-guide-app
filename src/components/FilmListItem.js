5198b61e3dbb5fedff7f89bd2b4d4acb1140f0d1
Tom Spencer <tom.spencer@studiozeffa.com>
2017-12-08 16:03:28 +0000

    Added film rating to list item

--- src/components/FilmListItem.js
+++ src/components/FilmListItem.js
@@ -3,10 +3,13 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const FilmListItem = ({ film }) => (
   <View style={styles.container}>
-    <Text style={styles.title}>{film.name}</Text>
-    <Text style={styles.showtime}>
-      {film.showtimes[0].startsAtDate} at {film.showtimes[0].startsAtTime} on {film.showtimes[0].channel}
-    </Text>
+    <View style={styles.titleContainer}>
+      <Text style={styles.title}>{film.name}</Text>
+      <Text style={styles.showtime}>
+        {film.showtimes[0].startsAtDate} at {film.showtimes[0].startsAtTime} on {film.showtimes[0].channel}
+      </Text>
+    </View>
+    {!!film.tmdbRating && <Text style={styles.rating}>{film.tmdbRating}%</Text>}
   </View>
 );
 
@@ -15,14 +18,23 @@ export default FilmListItem;
 const styles = StyleSheet.create({
   container: {
     padding: 12,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  titleContainer: {
+    flex: 1
   },
   title: {
     fontSize: 16
   },
   showtime: {
     fontSize: 14,
-    paddingTop: 4,
-    color: '#555'
+    paddingTop: 3,
+    color: '#666'
+  },
+  rating: {
+    fontSize: 16,
+    paddingLeft: 6
   }
 });
